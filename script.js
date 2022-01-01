61f97dbd71356f13fe503fa319949f97f9c16554
GuilhermeRinaldi <guilherme.rinaldi@outlook.com>
2022-01-01 17:34:44 -0300

    final

--- script.js
+++ script.js
@@ -14,64 +14,82 @@ function upperFirst(text, number) {
 
 	for (var i = length - 1; i >= 0; i--) {
 		const word = cutedText[i];
-		// let letterPosition = 0;
-		// while (word[letterPosition] === "\n" || " ") {
-		// 	letterPosition += 1;
-		// }
 		cutedText[i] = word.replace(word[0], word[0].toUpperCase());
 	}
-	return cutedText.join(" ")
+	return cutedText.join(" ");
 }
 
-function shake() {
-	document.getElementById("inputText").classList.add("shake");
-	setTimeout(function(){document.getElementById("inputText").classList.remove("shake")}, 200);
+const loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis venenatis laoreet nisl et porttitor. Sed in nisi at ipsum suscipit fermentum vitae venenatis augue. Proin ultrices faucibus erat vitae semper. Donec vitae nisi urna. Nam sagittis vel orci eleifend posuere. Aliquam eu consequat nisi. Curabitur ac facilisis metus, quis venenatis sapien. Donec orci dui, fermentum quis metus eget, dapibus scelerisque metus. Quisque feugiat et augue ut feugiat. Sed sollicitudin ullamcorper sem id mollis. Praesent vitae augue a ligula egestas efficitur. Aenean tempor dictum sodales. Donec vel magna placerat, fermentum justo viverra, venenatis ligula. Integer ac risus ut massa vehicula dignissim nec in quam. Nulla iaculis hendrerit velit, a interdum felis suscipit at. Etiam sodales pretium sem. \n";
+
+function loremGenerator(type,number) {
+	if (type === "paragraphs") {
+		return loremText.repeat(number);
+	}
+	if (type === "setences") {
+		return loremText.repeat(7).split(".",number).join(".");
+	}
+	if (type === "words") {
+		return loremText.split(" ",number).join(" ");
+	}
+}
+
+function shake(id) {
+	document.getElementById(id).classList.add("shake");
+	setTimeout(function(){document.getElementById(id).classList.remove("shake")}, 200);
 }
 
 document.querySelectorAll("button").forEach( function(button) {
     button.addEventListener("click", function(event) {
 
-   if (!!document.getElementById("inputText")){
-    	const inputText = document.getElementById("inputText").value;	
-    	if (inputText === "") {
-    	    shake();
-        }
-	    const button = event.target;
+    	const button = event.target;
 	    const id = button.id;
-
 	    try {
-		    if (id === "upper") {
-				write(inputText.toUpperCase());
+			if (id === "copyLorem") {
+				const quantity = document.getElementById("quantity").value;
+				const select = document.getElementById("select").value;
+				if (quantity <= 0 | quantity >= 100) {
+					shake("quantity");
+				}else {
+					navigator.clipboard.writeText(loremGenerator(select, quantity));
+				}
+				
 			}
-			if (id === "lower") {
-				write(inputText.toLowerCase());
-			}
-			if (id === "first") {
-				write(upperFirst(inputText, 1));
-			}
-			if (id === "capitalized") {
-				write(upperFirst(inputText, 0));
-			}
-			if (id === "clear") {
-				write("");
-			}
-			if (id === "copy") {
-				navigator.clipboard.writeText(inputText);
-			}
-		}
-	    catch{
-	    	shake();
 	    }
-	}
-	if (id === "copyLorem") {
-		const quantity = document.getElementById("quantity").value
-		const select = document.getElementById("select").value;
-		console.log(quantity);
-		console.log(select);
-		navigator.clipboard.writeText(select);
-	}
+	    catch {
+	    	shake("quantity");
+	    }
 
-  });
+	   if (!!document.getElementById("inputText")){
+	    	const inputText = document.getElementById("inputText").value;	
+	    	if (inputText === "") {
+	    	    shake("inputText");
+	        }
+
+		    try {
+			    if (id === "upper") {
+					write(inputText.toUpperCase());
+				}
+				if (id === "lower") {
+					write(inputText.toLowerCase());
+				}
+				if (id === "first") {
+					write(upperFirst(inputText, 1));
+				}
+				if (id === "capitalized") {
+					write(upperFirst(inputText, 0));
+				}
+				if (id === "clear") {
+					write("");
+				}
+				if (id === "copy") {
+					navigator.clipboard.writeText(inputText);
+				}
+			}
+		    catch{
+		    	shake("inputText");
+		    }
+		}
+  	});
 });
 
 
